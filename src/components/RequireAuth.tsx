6eb0707afefc481725723a7925b42c5f5e3fc59d
lsun <lsun.github@gmail.com>
2024-10-02 23:18:25 +0200

    fix require auth

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -12,15 +12,20 @@ export function RequireAuth({ children }: { children: JSX.Element }) {
         const user = await getCurrentUser();
         if (user) {
           setIsAuthenticated(true);
-          console.log("User is connected!!", user)
+          console.log("User is connected!!", user);
         }
       } catch (error) {
         setIsAuthenticated(false);
+        console.log(error);
       }
     };
     checkAuth();
   }, []);
 
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+  
   if (!isAuthenticated) {
     return <Navigate to="/signIn" state={{ from: location }} replace />;
   }
