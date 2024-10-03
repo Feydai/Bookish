57fb7fd796cf5b8f30ab04da86282f1273d18c12
lsun <lsun.github@gmail.com>
2024-10-03 10:52:14 +0200

    add: new feature home page not finish yet

--- src/routes/HomePage/HomePage.tsx
+++ src/routes/HomePage/HomePage.tsx
@@ -2,22 +2,35 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const HomePage: React.FC = () => {
-  const nav = useNavigate()
+  const nav = useNavigate();
   return (
-    <div className="flex items-center justify-center min-h-screen bg-purple-100 px-4 sm:px-6 lg:px-8">
-      <div className="bg-white rounded-xl shadow-lg p-8 max-w-xs sm:max-w-md lg:max-w-lg text-center relative">
-        <div className="bg-gray-200 rounded-full w-16 h-16 mx-auto mb-4 flex items-center justify-center">
+    <div className="flex items-center justify-center min-h-screen bg-[#f4f5f7]-100 px-4 sm:px-6 lg:px-8">
+      <div className="p-8 max-w-xs sm:max-w-md lg:max-w-lg text-center relative">
+        {/* <div className="bg-gray-200 rounded-full w-16 h-16 mx-auto mb-4 flex items-center justify-center">
           <span className="text-gray-600">LOGO</span>
-        </div>
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-purple-900 mb-2">
+        </div> */}
+        <h1 className="text-2xl sm:text-2xl lg:text-3xl font-bold text-black-900 mb-2">
           Welcome to Bookish
         </h1>
-        <p className="text-gray-600 mb-4 text-sm sm:text-base lg:text-lg">
-          Discover and discuss your favorite books with fellow readers.
+        <p className="text-gray-600 my-5 text-sm font-medium">
+          Connect, read, and discuss with hook lovers around the world.
         </p>
-        <button onClick={() => nav('/signIn')} className="bg-purple-600 text-white py-2 px-4 rounded-lg w-full mt-4 hover:bg-purple-700 transition duration-300">
-          Get Started !
+        <button
+          onClick={() => nav("/signIn")}
+          className="bg-[#07090c] text-white py-2 px-4 rounded-lg w-5/6 mt-4 transition duration-300"
+        >
+          Create Account
         </button>
+        <button
+          onClick={() => nav("/signIn")}
+          className="bg-[#ebebeb] text-black py-2 px-4 rounded-lg w-5/6 mt-4 transition duration-300"
+        >
+          Login
+        </button>
+        <div className="py-10 text-sm" >
+          <p>By continuing, you agree to our</p>
+          <a>By continuing, you agree to our Privacy Policy</a>
+        </div>
       </div>
     </div>
   );
